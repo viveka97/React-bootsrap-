07917a177373fa8fe4bfe91ccc0a0a166716c528
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2021-07-06 00:03:59 +0530

    s

---
+++ react-bootstrap-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarm from './components/navbar';
+
+function App() {
+  return (
+   <div className= "App">
+     <Navbarm/>
+
+   </div> 
+  );
+}
+
+export default App;
